04082ad245eac64f13b0395480d294a87656fab3
KodingForKittehs <kodingforkittehs@gmail.com>
2022-06-27 14:23:49 -0400

    Allow passing multiple args to terraform

--- terraform_docker.sh
+++ terraform_docker.sh
@@ -3,7 +3,7 @@
 docker run -it --rm \
     -v "${PWD}/terraform":/tfconfig \
     -u 1000:1000 \
-    hashicorp/terraform:latest -chdir=tfconfig "$1"
+    hashicorp/terraform:latest -chdir=tfconfig "$@"
 
 #docker run -it --rm \
 #    -v "${PWD}/terraform":/tfconfig \
